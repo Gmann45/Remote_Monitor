bd3baf2e3d7721deeb6410f2bd0324fa9644c1a7
Gmann45 <georgedederich@gmail.com>
2013-11-06 08:12:47 -0500

    Updated Arduino program, now uses motion sensor

--- Arduino_code_for_temp_and_motion/Arduino_code_for_temp_and_motion.ino
+++ Arduino_code_for_temp_and_motion/Arduino_code_for_temp_and_motion.ino
@@ -2,7 +2,14 @@
 The purpose of this code is to take temperature and motion readings from an Arduino 
 and send them to a computer via XBee shield with XBee for the Arduino and a dongle 
 with XBee to a serial port. The data will then be uploaded into a website using
-JavaScript. The temperature sensor used is an LM61.
+JavaScript. The temperature sensor used is an LM61 and the motion sensor used is a 
+PIR motion sensor from SparkFun.com.
+
+The temperature sensor is connected so Vs is connected to the 5V pin on the Arduino,
+the ground is connected to the the GND pin and the Vo is connected to A0. The motion
+sensor is connected so Vs is connected to the 5V pin on the Arduino, the ground is 
+connected to the the GND pin, and the Vo is connected to a pullup resistor which is 
+connected to A1.
 
 This project is for an assignment given by Prof. Dave Johnson in Networking 
 Technologies.
@@ -12,23 +19,39 @@ Date Created: 2013-10-29
 */
 
 static float temperature=0;
+static int timer=0;
 
   void setup(){
     Serial.begin(9600); //Setting up serial connection between the Arduino                         
   }                     //and the computer.
   
   void loop(){
-    
-    //if (Serial.available() >0) { //Telling the program to proceed if there is
-                                 //serial connection available.
-     
-      for (int i=0; i<100; i++){               //Averaging temperature to get a more accurate
+   
+      for (int i=0; i<20; i++){               //Averaging temperature to get a more accurate
         temperature=temperature+analogRead(0); //temperature reading.
       }
       
-      temperature=temperature/100.0; //Dividing the reading by amount of times averaged
+      temperature=temperature/20.0;           //Dividing the reading by amount of times averaged
       temperature=((temperature*(5000.0/1024.0))-600.0)/10;
-      Serial.println(temperature);     //Sends temperature in C to serial port
+      Serial.print("Temperature = ");
+      Serial.println(temperature);       //Sends temperature in C to serial port
+      
+      if (analogRead(1) != 0){               //Motion sensor is connected to pin 1, sends a 0 when no motion is detected
+        timer=timer+1;        //Counts to a set value to determine whether the room is occupied, 10 mins will be set at 600
+        if (timer < 11){
+          Serial.println(timer); //Sends timer values if determining if room is occupied
+        }
+        if (timer > 10){             
+          Serial.println("Room is unoccupied"); //Sends alert that the room is unoccupied
+        }
+      }
+       else {
+         timer=0;
+         Serial.println("Room is occupied"); //Sends alert that the room is occupied when movement is sensed
+       }
+      
+      
+
       delay(1000);                   //Delay the loop by 1 second
       temperature = 0;          
     };
